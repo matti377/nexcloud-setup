1023d8f32c0e1c6f03cc945e5f3bbed61a94d4fd
Mattti Wirtz <119162325+matti377@users.noreply.github.com>
2023-05-26 11:17:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,10 +12,22 @@ sudo apt install php-gd php-mysql php-curl php-json php-gmp php-bcmath php-xml p
 sudo nano /etc/php/7.4/apache2/php.ini
 change to memory_limit = 512M
 sudo systemctl restart apache2
+
+#configure Mysql
+sudo mysql -u root -p
+CREATE DATABASE nextcloud_db;
+GRANT ALL ON nextcloud_db.* to 'nextcloud_user'@'localhost' IDENTIFIED BY 'userpassword';
+FLUSH PRIVILEGES;
+exit;
+
 #download nextclod
-
+wget https://download.nextcloud.com/server/releases/latest.zip
+sudo unzip nextcloud-21.0.1.zip -d /var/www/html/
+sudo chown -R www-data:www-data /var/www/html/nextcloud
+sudo chmod -R 775 /var/www/html/nextcloud
 #move to correct directory
-
+sudo vim /etc/apache2/sites-available/nextcloud.conf
+sudo a2ensite nextcloud.conf
 #change permissions
 
 #add trusted domains
