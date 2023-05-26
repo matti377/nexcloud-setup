0ed9a139846c958ab3937b2cf766b4f784b33581
Mattti Wirtz <119162325+matti377@users.noreply.github.com>
2023-05-26 11:09:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ echo "this script will install and configure Mysql"
 echo"please provide me the details for the mysql installation"
 
 #download dependencies
+sudo apt install apache2 libapache2-mod-php php mariadb-server -y
+sudo apt install php-gd php-mysql php-curl php-json php-gmp php-bcmath php-xml php-mbstring php-intl php-imagick php-zip bzip2 -y
 
 #download nextclod
 
