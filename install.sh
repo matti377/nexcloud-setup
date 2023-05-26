b4436c1896a6b5c17094a1b2635375d88bd90f8d
Mattti Wirtz <119162325+matti377@users.noreply.github.com>
2023-05-26 11:08:09 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+echo "This script will set up nextcloud with Apache2 and mysql"
+echo "To start please provide some details"
+#ask for credentials for mysql
+echo "this script will install and configure Mysql"
+echo"please provide me the details for the mysql installation"
+
+#download dependencies
+
+#download nextclod
+
+#move to correct directory
+
+#change permissions
+
+#add trusted domains
